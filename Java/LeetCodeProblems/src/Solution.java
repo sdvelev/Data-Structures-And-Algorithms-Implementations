4fa28df18ad873670593cda836f75655dbdc0345
Stefan Velev <sdvelev@gmail.com>
2024-02-23 17:15:25 +0200

    Added solution to next programming task in Java

--- Java/LeetCodeProblems/src/Solution.java
+++ Java/LeetCodeProblems/src/Solution.java
@@ -291,6 +291,34 @@ public class Solution {
         return true;
     }
 
+    public static int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
         // Intersect Problem
         int[] resultIntersect = Solution.intersect(new int[]{1, 2, 2, 1}, new int[]{2, 2});
@@ -351,5 +379,8 @@ public class Solution {
             {'.','.', '.','4','1','9','.','.','5'},
             {'.','.', '.','.','8','.','.','7','9'},
         }));
+
+        // Search in Rotated Sorted Array
+        System.out.println(Solution.search(new int[] {4, 5, 6, 7, 0, 1, 2}, 0));
     }
 }
